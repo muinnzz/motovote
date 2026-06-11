8b0c6eeee45d8617997fec88bde8032c59f5044f
muinnzz <konohagakuredes@gmail.com>
2026-06-11 18:56:57 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -13,24 +13,21 @@ function update(){
 
 let total = votes.Y15ZR + votes.RS150R + votes.RXZ;
 
-document.getElementById("y15count").innerText = votes.Y15ZR + " Undi";
-document.getElementById("rscount").innerText = votes.RS150R + " Undi";
-document.getElementById("rxzcount").innerText = votes.RXZ + " Undi";
+document.getElementById("y15count").innerText = votes.Y15ZR+" undi";
+document.getElementById("rscount").innerText = votes.RS150R+" undi";
+document.getElementById("rxzcount").innerText = votes.RXZ+" undi";
 
 let y15 = total ? votes.Y15ZR/total*100 : 0;
 let rs = total ? votes.RS150R/total*100 : 0;
 let rxz = total ? votes.RXZ/total*100 : 0;
 
+document.getElementById("barY15").style.width = y15+"%";
+document.getElementById("barRS").style.width = rs+"%";
+document.getElementById("barRXZ").style.width = rxz+"%";
+
 document.getElementById("y15percent").innerText = y15.toFixed(1)+"%";
 document.getElementById("rspercent").innerText = rs.toFixed(1)+"%";
 document.getElementById("rxzpercent").innerText = rxz.toFixed(1)+"%";
-
-let ranking = Object.entries(votes).sort((a,b)=>b[1]-a[1]);
-
-document.getElementById("ranking").innerHTML =
-ranking.map((v,i)=>`
-<p>${["🥇","🥈","🥉"][i]} ${v[0]} - ${v[1]}</p>
-`).join("");
 
 }
 
